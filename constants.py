2fa24d797a0fb5ce4840af7c138ee014c03a5e7d
Daniel <danielgfree@gmail.com>
2015-01-01 18:26:32 -0500

    Push initial program.

---
+++ constants.py
@@ -0,0 +1,26 @@
+SCREEN_HEIGHT = 255
+SCREEN_WIDTH = 255
+
+GRID_WIDTH = 20
+GRID_HEIGHT = 20
+MARGIN = 5
+
+ROWS = 10
+COLUMNS = 10
+
+# Number of mines to place on the grid
+MINES = 10
+
+# Define some colors
+BLACK    = (   0,   0,   0)
+WHITE    = ( 255, 255, 255)
+DARKGREEN= (   0, 100,   0)
+GREEN    = (   0, 255,   0)
+LITEGREEN= ( 144, 238, 144)
+RED      = ( 255,   0,   0)
+BLUE     = (   0,   0, 255)
+CYAN     = (   0, 255, 255)
+PURPLE   = ( 128,   0, 128)
+PINK     = ( 255, 130, 171)
+YELLOW   = ( 255, 255,   0)
+ORANGE   = ( 255, 165,   0)
