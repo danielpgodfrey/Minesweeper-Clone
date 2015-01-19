98fbe5e38cb5b1473d4c2d53739dc806a5375224
Daniel <danielgfree@gmail.com>
2015-01-18 20:59:15 -0500

    __str__ method

--- grid.py
+++ grid.py
@@ -1,54 +1,78 @@
 import random
 from block import Block
-from constants import *
+
 
 class Grid:
-    """
-    This class handles the grid of the game and its properties. It is
+    """This class handles the grid of the game and its properties. It is
     responsible for generating elements associated with the grid like
     mines and Blocks, as well as some properties associated with the Block.
 
     Parameters:
     blocks = 2D list of Block objects.
-    self.row_count = number of rows in the grid
-    self.col_count = number of columns in the grid
+    row_count = number of rows in the grid
+    col_count = number of columns in the grid
+    mine_count = number of mines in the grid
     """
     def __init__(self, click_position):
         # Default to beginner game
         self.row_count = 9
         self.col_count = 9
         self.mine_count = 10
-        
+
         self.initialize_grid(click_position)
 
+    def __str__(self):
+        block_list = list()
+
+        for row in self.blocks:
+            for block in row:
+                if block.is_mine:
+                    block_list.append('M')
+                elif block.is_revealed and block.mine_neighbor_count == 0:
+                    block_list.append('~')
+                else:
+                    block_list.append(str(block.mine_neighbor_count))
+            block_list.append('\n')
+
+        return ''.join(block_list)
+
     def initialize_grid(self, click_position):
+        """Create mines and blocks based on initial click position.
+        Reveal the blocks around the initial clicked block.
+
+        Keyword arguments:
+        click_position - tuple containing the x-pixel and y-pixel that the
+        mouse has clicked
+        """
         row, col = self._find_clicked_row_col(click_position)
         mines = self._generate_mines(row, col)
         mine_grid = self._calc_mine_grid(mines)
         mine_neighbor_grid = self._calc_mine_neighbors(mines)
 
         self.blocks = [[Block(mine_grid[x][y], mine_neighbor_grid[x][y])
-            for x in range(self.row_count)] for y in range(self.col_count)]
+                       for x in range(self.row_count)]
+                       for y in range(self.col_count)]
 
         self._reveal_around(row, col)
 
     def click_grid(self, click_position, click_type):
+        """Updates the grid based on the a mouse click.
+        Takes in
         """
-        Updates the grid based on the click.
-        """
-
         row, col = self._find_clicked_row_col(click_position)
         if click_type == "left":
             self.blocks[row][col].reveal()
             self.blocks[row][col].draw()
             if self.blocks[row][col].mine_neighbors == 0:
-                self.reveal_around(row, column)
+                self.reveal_around(row, col)
 
         elif click_type == "right":
             self.blocks[row][col].cycle_flag()
             self.blocks[row][col].draw()
 
-    def update_grid(self, row_count, col_count, mine_count):
+    def update_params(self, row_count, col_count, mine_count):
+        """Change the parameters of the grid.
+        """
         self.row_count = row_count
         self.col_count = col_count
         self.mine_count = mine_count
@@ -71,16 +95,16 @@ class Grid:
 
     def _generate_mines(self, row_clicked, col_clicked):
         """
-        Creates mines for the grid. This will not create any mines within
+        Create mines for the grid. This will not create any mines within
         1 block of the block that the user clicked.
 
-        Returns a 2D list of coordinates corresponding to the
+        Return a 2D list of coordinates corresponding to the
         locations of mines.
         """
         mines = random.sample(range(self.col_count * self.row_count),
-            self.mine_count + 8)
+                              self.mine_count + 8)
 
-        # Compose the integers into grid rows and self.col_count
+        # Compose the integers into grid rows and columns
         mine_rows = [x // self.col_count for x in mines]
         mine_cols = [x % self.col_count for x in mines]
         mine_grid = set(zip(mine_rows, mine_cols))
@@ -99,7 +123,7 @@ class Grid:
 
     def _calc_mine_grid(self, mines):
         mine_grid = [[False for x in range(self.row_count)]
-            for y in range(self.col_count)]
+                     for y in range(self.col_count)]
 
         for mine in mines:
             mine_grid[mine[0]][mine[1]] = True
@@ -108,12 +132,12 @@ class Grid:
 
     def _calc_mine_neighbors(self, mines):
         """
-        Takes a list of 2-tuples corresponding to coordinates of
-        mine locations and returns a 2D list of integers corresponding
+        Take a list of 2-tuples corresponding to coordinates of
+        mine locations and return a 2D list of integers corresponding
         to the mine neighbor count of all of the blocks.
         """
         mine_neighbor_grid = [[0 for x in range(self.row_count)]
-            for y in range(self.col_count)]
+                              for y in range(self.col_count)]
 
         for mine in mines:
             mine_row = mine[0]
@@ -129,20 +153,27 @@ class Grid:
 
     def _find_neighbors(self, row, col):
         """
-        Returns a set of 2-tuples containing the neighbors of the
-        input row and column. (Returns set that just +/- 1 for row and col
-        and makes sure that it isn't negative.)
+        Return a set of 2-tuples containing the neighbors of the
+        input row and column. (Return set that is just +/- 1 of row and col
+        and make sure that it isn't negative or past the number of rows.)
         """
         neighbors = {(row + x, col + y)
-            for x in range(-1, 2) for y in range(-1, 2)}
+                     for x in range(-1, 2) for y in range(-1, 2)}
 
         # Take into account edge and corner cases (literally!)
         return {neighbor for neighbor in neighbors
-            if neighbor[0] >= 0 and neighbor[1] >= 0
-            and neighbor[0] < self.row_count and neighbor[1] < self.col_count}
+                if neighbor[0] >= 0 and neighbor[1] >= 0
+                and neighbor[0] < self.row_count and
+                neighbor[1] < self.col_count}
 
     def _find_clicked_row_col(self, click_position):
-        column_clicked = click_position[0] // (Block.BLOCK_WIDTH + Block.MARGIN)
+        """
+        Convert the tuple of a mouse click position (given in pixels, x
+        and y position) to a tuple of block position on the grid.
+        """
+        column_clicked = click_position[0] // \
+            (Block.BLOCK_WIDTH + Block.MARGIN)
+
         row_clicked = click_position[1] // (Block.BLOCK_HEIGHT + Block.MARGIN)
 
         # If the user clicks below the final row, count it as the final row.
@@ -154,22 +185,10 @@ class Grid:
         return row_clicked, column_clicked
 
 if __name__ == "__main__":
-    def print_grid(grid):
-        for row in grid.blocks:
-            for block in row:
-                if block.is_mine:
-                    print("M", end="")
-                elif block.is_revealed and block.mine_neighbor_count==0:
-                    print("~",sep="",end="")
-                else:
-                    print(block.mine_neighbor_count,sep="",end="")
-            print()
-        print()
-    
-    grid = Grid((120,120))
-    print_grid(grid)
+    # Demo
+    grid = Grid((120, 120))
+    print(grid)
 
-    grid.update_grid(20, 20, 40)
-    grid.initialize_grid((120,120))
-    print_grid(grid)
-    
+    grid.update_params(20, 20, 40)
+    grid.initialize_grid((120, 120))
+    print(grid)
